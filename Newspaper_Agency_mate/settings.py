ac3e975cdc02f62f5b2446461adf411b4a6152d0
semermaks <semermaks@mail.com>
2024-05-16 12:22:56 +0300

    User model settings

--- Newspaper_Agency_mate/settings.py
+++ Newspaper_Agency_mate/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'newspappers'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'newspappers.Redactor'
