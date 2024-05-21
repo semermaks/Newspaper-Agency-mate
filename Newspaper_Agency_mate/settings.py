d323f35391b756226a101268611f649640c3f747
semermaks <semermaks@mail.com>
2024-05-21 13:31:44 +0300

    Add api for deployment

--- Newspaper_Agency_mate/settings.py
+++ Newspaper_Agency_mate/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-dj#*4*+$yn8y*5cr0@e$a$!0p$(^k!1478wqmbatg54a@7&uci')
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
