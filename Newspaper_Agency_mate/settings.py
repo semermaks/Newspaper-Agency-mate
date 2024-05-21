7690bae956b654c1743dcf5ad871b925424b3c2f
semermaks <semermaks@mail.com>
2024-05-21 14:33:59 +0300

    Add host to ALLOWED_HOSTS

--- Newspaper_Agency_mate/settings.py
+++ Newspaper_Agency_mate/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["newspaper-agency-mate-hlbp.onrender.com", "127.0.0.1"]
 
 
 # Application definition
