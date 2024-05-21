69205d561c9557efd49a50db5f46c4a287375bb3
semermaks <semermaks@mail.com>
2024-05-21 11:27:24 +0300

    Update settings & requirements.txt to deploy

--- Newspaper_Agency_mate/settings.py
+++ Newspaper_Agency_mate/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dj#*4*+$yn8y*5cr0@e$a$!0p$(^k!1478wqmbatg54a@7&uci'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-dj#*4*+$yn8y*5cr0@e$a$!0p$(^k!1478wqmbatg54a@7&uci')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -134,3 +134,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
+
